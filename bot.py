dac47854678a9daa45b85c8a88aa55f9cfed7dfa
saakyanarchi-design <saakyan.archi@gmail.com>
2026-07-09 02:32:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,7 +64,8 @@ def init_google_sheets():
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
             "token_uri": "https://oauth2.googleapis.com/token",
             "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL")
+            "client_x509_cert_url": os.getenv("CLIENT_CERT_URL") or os.getenv("CLIENT_X509_CERT_URL")
+
         }
 
         # Проверка обязательных полей
